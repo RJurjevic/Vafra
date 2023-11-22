f2f89dad9b347e0bd6c012e066969664283d2c5d
Robert Jurjevic <72772239+RJurjevic@users.noreply.github.com>
2023-11-22 00:32:45 +0000

    nn-dbbd35556e55.nnue
    V 14.12.1
     - Used nn-dbbd35556e55.nnue NNUE aka nn-v0f000010111.nnue.

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,7 @@ namespace PSQT {
 
   V 14.12.1
     - Stockfish 12 modified so to use flipped HalfKP 256x2-64-32-1 NNUE.
-    - Used nn-57d1331fc66d.nnue NNUE aka nn-v0f000010109.nnue.
+    - Used nn-dbbd35556e55.nnue NNUE aka nn-v0f000010111.nnue.
 */
 
 int main(int argc, char* argv[]) {
