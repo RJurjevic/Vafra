5b39083f2acd4e1b910a7e06fab8bc3c46b25a08
Robert Jurjevic <72772239+RJurjevic@users.noreply.github.com>
2023-12-17 17:54:50 +0000

    nn-0f93959f0d44.nnue
    V 14.12.1
     - Used nn-0f93959f0d44.nnue NNUE aka nn-v0f000010304.nnue.

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,7 @@ namespace PSQT {
 
   V 14.12.1
     - Stockfish 12 modified so to use flipped HalfKP 512x2-32-32-1 NNUE.
-    - Used nn-7c983bef22cd.nnue NNUE aka nn-v0f000010303.nnue.
+    - Used nn-0f93959f0d44.nnue NNUE aka nn-v0f000010304.nnue.
 */
 
 int main(int argc, char* argv[]) {
