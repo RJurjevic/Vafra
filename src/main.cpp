5aae702415df6bb1d08d0f64d02cfa4a213934b7
Robert Jurjevic <72772239+RJurjevic@users.noreply.github.com>
2024-02-27 19:59:21 +0000

    nn-dda023920c53.nnue
    current changes between V 14.12.0 and V 14.12.1
    
    V 14.12.1
     - Used nn-dda023920c53.nnue NNUE aka nn-v0f000010902.nnue.
     - In search Step 16 Reduced depth search removed ttHit running average from do LMR criteria.
    
    in this commit
    
    V 14.12.1
     - Used nn-dda023920c53.nnue NNUE aka nn-v0f000010902.nnue.

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ namespace PSQT {
     - In Step 16 Reduced depth search removed decreasing reduction if the ttHit running average is large.
 
   V 14.12.1
-    - Used nn-ad7789b2d523.nnue NNUE aka nn-v0f000010901.nnue.
+    - Used nn-dda023920c53.nnue NNUE aka nn-v0f000010902.nnue.
     - In search Step 16 Reduced depth search removed ttHit running average from do LMR criteria.
 */
 
