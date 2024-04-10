356ee64a99a5310b21574b76a4913254571fa74f
Robert Jurjevic <72772239+RJurjevic@users.noreply.github.com>
2024-04-10 22:26:27 +0100

    new nn-7bbf561d7612.nnue aka nn-v0f000010910.nnue
    Vafra 14.12.2(5)
    
    V 14.12.2
     - Used nn-7bbf561d7612.nnue NNUE aka nn-v0f000010910.nnue.
     - In search Step 16 Reduced depth search LMR not done if the move is extended.

--- src/evaluate.h
+++ src/evaluate.h
@@ -38,7 +38,7 @@ namespace Eval {
   // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
   // for the build process (profile-build and fishtest) to work. Do not change the
   // name of the macro, as it is used in the Makefile.
-  #define EvalFileDefaultName   "nn-622d6e793eaf.nnue"
+  #define EvalFileDefaultName   "nn-7bbf561d7612.nnue"
 
   namespace NNUE {
 
