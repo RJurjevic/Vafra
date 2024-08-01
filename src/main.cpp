31ef97a93508043cbb4c60532a39bf12c7ed0a58
Robert Jurjevic <72772239+RJurjevic@users.noreply.github.com>
2024-08-01 18:34:20 +0100

    new net
    current changes between V 14.12.2 and V 14.12.1
    
    V 14.12.2
     - Used nn-fd48199ed27d.nnue NNUE aka nn-v0f000011204.nnue.
     - In search Step 16 Reduced depth search LMR is not applied if the move is extended.
     - In search Step 11 Internal iterative deepening now calculates ttValue.
     - In Eval::evaluate switched to pure NNUE evaluation, moving away from the hybrid approach.
    
    in this commit
    
    V 14.12.2
     - Used nn-fd48199ed27d.nnue NNUE aka nn-v0f000011204.nnue.

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ namespace PSQT {
     - In search Step 16 Reduced depth search removed ttHit running average from do LMR criteria.
 
   V 14.12.2
-    - Used nn-7f1489a0278c.nnue NNUE aka nn-v0f000011203.nnue.
+    - Used nn-fd48199ed27d.nnue NNUE aka nn-v0f000011204.nnue.
     - In search Step 16 Reduced depth search LMR is not applied if the move is extended.
     - In search Step 11 Internal iterative deepening now calculates ttValue.
     - In Eval::evaluate switched to pure NNUE evaluation, moving away from the hybrid approach.
